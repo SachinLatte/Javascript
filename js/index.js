84209b003eb7e35183a7865218e04a9f13b23366
SachinLatte <sachin@digitallatte.in>
2026-01-07 18:35:06 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// let welcomeMessage = "Hello, welcome to learning JavaScript!";
+// console.log(welcomeMessage);
+let cartQuantity = 0;
+let addBtn = document.getElementById("addBtn");
+document.getElementById('heading').innerText = `Your bag has ${cartQuantity} items`;
+// addBtn.addEventListener("click",function(){
+// cartQuantity++;
+// });
