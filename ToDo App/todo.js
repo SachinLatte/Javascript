7175b3cd4d8b70456619658528a72d32bf47d2f0
SachinLatte <sachin@digitallatte.in>
2026-01-20 19:16:46 +0530

    todo app delete functionality added

--- ToDo App/todo.js
+++ ToDo App/todo.js
@@ -1,10 +1,22 @@
-let todoList = ['Buy Milk','Go To College'];
+let todoList = [
+                {
+                  items:'Buy Milk', 
+                  dueDate: '20/01/2026'
+                },
+                {
+                  items:'Buy Fruits', 
+                  dueDate: '20/01/2026'
+                }
+              ];
 displayItems();
 function addTodo(){
   let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
   let todoItem = inputElement.value;
-  todoItem !== '' ? todoList.push(todoItem) : alert('Please give some input');  
+  let todoDate = dateElement.value;
+  todoItem !== '' ? todoList.push({items : todoItem, dueDate: todoDate}) : alert('Please give some input');  
   inputElement.value = '';
+  dateElement.value = '';
   inputElement.focus();
   console.log(todoList);
   displayItems();
@@ -14,12 +26,14 @@ function displayItems(){
   let containerElement = document.querySelector('.todo-container');
   let newHTML = '';
   for(let i =0; i < todoList.length; i++){
+    let {items, dueDate} = todoList[i];
     newHTML += `
-    <div>
-    <span>${todoList[i]}</span>
-    <button>Delete</button>
-    </div>
+    <span>${items}</span>
+    <span>${dueDate}</span>
+    <button onclick="todoList.splice(${i}, 1); displayItems()">Delete</button>
     `;
   }
   containerElement.innerHTML = newHTML;
 }
+
+console.log(todoList);
